87b3a2b53e40f5dc96153b9edcf6f211410d1e76
zackattacknyu <zackattacknyu@gmail.com>
2014-03-06 17:36:38 -0800

    got a crude basic reading of serial data working

--- JMonkeyEngine/TestProbeGame/src/mygame/Main.java
+++ JMonkeyEngine/TestProbeGame/src/mygame/Main.java
@@ -81,6 +81,8 @@ public class Main extends SimpleApplication {
         for(String line: lines){
             path.addWayPoint(lineToWayPoint(line));
         }
+        System.out.println("ZACH'S LINE");
+        SerialTest.executeMain();
         //path.addWayPoint(new Vector3f(5.5900f,1.6770f,27.9500f));
         //path.addWayPoint(new Vector3f(-3.0130f,1.6770f,22.8080f));
         //path.addWayPoint(new Vector3f(-11.6160f,1.6770f,27.9500f));
