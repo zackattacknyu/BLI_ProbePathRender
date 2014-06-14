b36f7d3ba575a31b9378b0742c81528188ba711b
zackattacknyu <zackattacknyu@gmail.com>
2014-06-13 17:24:47 -0700

    added new helper class

---
+++ JMonkeyEngine/TestProbeGame/src/util/GeneralHelper.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import com.jme3.math.Matrix3f;
+import com.jme3.math.Quaternion;
+import com.jme3.math.Vector3f;
+
+/**
+ *
+ * @author BLI
+ */
+public class GeneralHelper {
+
+    public static Matrix3f getRotationMatrix(float angle, Vector3f axis) {
+        Quaternion rotation = new Quaternion();
+        rotation.fromAngleAxis(angle, axis);
+        return rotation.toRotationMatrix();
+    }
+    
+}
