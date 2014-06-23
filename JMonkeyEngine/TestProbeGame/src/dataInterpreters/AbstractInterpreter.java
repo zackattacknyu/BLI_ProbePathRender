e57ba15de33362183d2c37bde21b847500b27ad4
zackattacknyu <zackattacknyu@gmail.com>
2014-06-22 20:04:33 -0700

    added shell classes to be filled in later

---
+++ JMonkeyEngine/TestProbeGame/src/dataInterpreters/AbstractInterpreter.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataInterpreters;
+
+/**
+ * This will be the super-class for all classs which take raw
+ *      data and decide what to pass to the tracking class
+ * @author BLI
+ */
+public class AbstractInterpreter {
+    
+}
---
+++ JMonkeyEngine/TestProbeGame/src/dataInterpreters/KeyboardMouseInterpreter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dataInterpreters;
+
+/**
+ *
+ * This will mostly be a shell class
+ *      that takes the keyboard and mouse
+ *      input and interprets what should be passed
+ *      to the tracker class
+ * 
+ * @author BLI
+ */
+public class KeyboardMouseInterpreter {
+    
+}
