c28bcac87b5124e941f9a620ea42fc322e4bbe8e
zackattacknyu <zackattacknyu@gmail.com>
2014-06-22 18:31:41 -0700

    finished javadoc of code

--- JMonkeyEngine/TestProbeGame/src/cameraImpl_ProbePathRender/CameraTrackerImpl_ProbePathRender.java
+++ JMonkeyEngine/TestProbeGame/src/cameraImpl_ProbePathRender/CameraTrackerImpl_ProbePathRender.java
@@ -17,21 +17,31 @@ import com.jme3.renderer.Camera;
  */
 public class CameraTrackerImpl_ProbePathRender extends CameraTrackerImpl{
     
+    //default camera when viewing the sphere
     public static final Vector3f SPHERE_MODE_DEFAULT_LOCATION = 
             new Vector3f(-22.649244f, -17.260416f, -67.74668f);
     public static final Quaternion SPHERE_MODE_DEFAULT_ROTATION = 
             new Quaternion(0.17899777f, 0.0838113f, 0.95806247f, -0.20748913f);
     
+    //default camera when viewing lola, the most common mode
     public static final Vector3f LOLA_MODE_DEFAULT_LOCATION = 
             new Vector3f(-16.928802f, 23.251862f, -54.489124f);
     public static final Quaternion LOLA_MODE_DEFAULT_ROTATION = 
             new Quaternion(0.20308718f, 0.20007013f, -0.042432234f, 0.9575631f);
     
+    //default camera when viewing raw data
     public static final Vector3f RAW_DATA_MODE_DEFAULT_LOCATION = 
             new Vector3f(-0.67807275f, 0.5436802f, -2.7487648f);
     public static final Quaternion RAW_DATA_MODE_DEFAULT_ROTATION = 
             new Quaternion(0.15252174f, -2.9824104E-4f, 0.7722618f, 0.61672425f);
     
+    /**
+     * Initializes the camera for the PathProbeRender application.
+     *      This includes methods that set its default camera
+     * @param currentCam            camera object
+     * @param currentFlyCam         fly camera object
+     * @param manager               input manager
+     */
     public CameraTrackerImpl_ProbePathRender(Camera currentCam, FlyByCamera currentFlyCam, InputManager manager){
         super(currentCam,currentFlyCam,manager);
     }
