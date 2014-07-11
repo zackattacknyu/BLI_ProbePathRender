5ee456871b081cd2a38b36b328e5fc0af5c71d24
zackattacknyu <zackattacknyu@gmail.com>
2014-07-10 17:43:10 -0700

    started process of modularizing the path following mesh operation

---
+++ JMonkeyEngine/TestProbeGame/src/org/zrd/graphicsTools/geometry/meshTraversal/PathFollowingMeshOperation.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.zrd.graphicsTools.geometry.meshTraversal;
+
+import com.jme3.math.Vector3f;
+import java.util.ArrayList;
+
+/**
+ * This houses the code that takes in a mesh and a path
+ *      as well as the target start and end points
+ *      on the mesh and tries to make the path follow 
+ *      the mesh so that the end points match
+ *
+ * @author BLI
+ */
+public class PathFollowingMeshOperation {
+    
+    //first path inputted
+    private ArrayList<Vector3f> originalPath;
+    
+    //path moved so that its start point is the same as target start point
+    private ArrayList<Vector3f> initiallyMovedPath;
+}
