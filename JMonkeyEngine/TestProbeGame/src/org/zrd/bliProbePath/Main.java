a4b22810dd409156e659b34429df41eee9ec87d2
zackattacknyu <zackattacknyu@gmail.com>
2014-07-15 15:28:52 -0700

    cleaned up unused code in old Main

--- JMonkeyEngine/TestProbeGame/src/org/zrd/bliProbePath/Main.java
+++ JMonkeyEngine/TestProbeGame/src/org/zrd/bliProbePath/Main.java
@@ -4,9 +4,6 @@ import org.zrd.geometryToolkit.geometryUtil.ProgramConstants;
 import org.zrd.cameraTracker.cameraMoves.CameraTracker;
 import com.jme3.app.SimpleApplication;
 import com.jme3.font.BitmapText;
-import com.jme3.input.KeyInput;
-import com.jme3.input.controls.ActionListener;
-import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
 import com.jme3.math.ColorRGBA;
 import com.jme3.renderer.RenderManager;
@@ -24,8 +21,6 @@ import org.zrd.bliProbePath.renderedObjects.SphereMesh;
 import org.zrd.geometryToolkit.meshDataStructure.TriangleSet;
 import org.zrd.geometryToolkit.pathDataStructure.RecordedPathSet;
 import org.zrd.graphicsToolsImpl.pathImpl.PathRenderHelper;
-import org.zrd.graphicsToolsImpl.pathImplDebug.PathXYDataDisplay;
-import org.zrd.graphicsToolsImpl.pathImplDebug.PathYawPitchRollDataDisplay;
 import org.zrd.jmeUtil.materials.MaterialHelper;
 import org.zrd.pathInteractions.PathImport;
 import org.zrd.probeTracking.ProbeTracker;
@@ -42,7 +37,7 @@ import org.zrd.probeTrackingOnSurface.ResetTracker;
 public class Main extends SimpleApplication {
     
     private Spatial surface,moveableObject;
-    private Material lineMaterial,redMaterial,orangeMaterial,greenMaterial;
+    private Material lineMaterial;
     
     private BitmapText yawPitchRollText, xyzText, recordingText, resetProbeText, probeMoveModeText;
     
@@ -59,11 +54,6 @@ public class Main extends SimpleApplication {
     //this is if we are using the sphere for testing 
     //      instead of lola
     private boolean sphereOn = false;
-    
-    //if we want to display the raw data instead of 
-    //      the sphere or lola mesh. This overrides
-    //      the above setting.
-    private boolean displayRawDataMode = true;
     
     //to be used if we are only testing using recorded paths
     private boolean showProbeRepLines = true;
@@ -110,9 +100,7 @@ public class Main extends SimpleApplication {
         initialImportDirectory = Paths.get("C:\\Users\\BLI\\Desktop\\BLI_ProbePathRender\\sampleTextFiles").toFile();
         
         cameraTracker = new CameraTrackerImpl_ProbePathRender(cam,flyCam,inputManager);
-        if(displayRawDataMode){
-            cameraTracker.setDefaultCamera((short)2);
-        }else if(sphereOn){
+        if(sphereOn){
             cameraTracker.setDefaultCamera((short)0);
         }else{
             cameraTracker.setDefaultCamera((short)1);
@@ -124,20 +112,17 @@ public class Main extends SimpleApplication {
         }else{
             activeMesh = new LolaMesh(assetManager);
         }
-        
-        redMaterial = MaterialHelper.makeColorMaterial(assetManager,ColorRGBA.Red);
-        orangeMaterial = MaterialHelper.makeColorMaterial(assetManager,ColorRGBA.Orange);
-        greenMaterial = MaterialHelper.makeColorMaterial(assetManager,ColorRGBA.Green);
+
         lineMaterial = MaterialHelper.makeColorMaterial(assetManager,ColorRGBA.Black);
 
         probeTracker = ProbeTracker_BLIProbePath.createNewProbeTracker(inputManager);
         
         moveableObject = ProbeRepresentation.getProbeRepresentation(assetManager);
-        if(!displayRawDataMode && showProbeRepLines){
+        if(showProbeRepLines){
             rootNode.attachChild(moveableObject);
         }
         
-        if(!sphereOn && !displayRawDataMode){
+        if(!sphereOn){
             rootNode.attachChild(BackgroundBox.getBackgroundBox(assetManager));
         }
 
@@ -145,15 +130,13 @@ public class Main extends SimpleApplication {
         surface = activeMesh.getSurfaceMesh();
         
         
-        initKeyboardInputs();
         initDebugText();
         
         shootables = new Node("shootables");
         shootables.attachChild(surface);
         
-        if(!displayRawDataMode){
-            rootNode.attachChild(shootables);
-        }
+        rootNode.attachChild(shootables);
+        
         recordedPathSet = new RecordedPathSet();
         
         lineMoveAction = new LineMoveAction(inputManager, cam, shootables, recordedPathSet, meshInfo);
@@ -197,6 +180,8 @@ public class Main extends SimpleApplication {
     
     private void initDebugText(){
         
+        guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
+        
         float currentStartY = 0.0f;
         
         recordingText = initializeNewText(currentStartY);
@@ -231,37 +216,5 @@ public class Main extends SimpleApplication {
     
     private void displayPath(Spatial path){
         rootNode.attachChild(path);
-    }
-    
-    private void initKeyboardInputs() {
-        guiFont = assetManager.loadFont("Interface/Fonts/Default.fnt");
-        
-        inputManager.addMapping("rawXYdisplay", new KeyTrigger(KeyInput.KEY_T));
-        inputManager.addMapping("rawYawPitchRollDisplay", new KeyTrigger(KeyInput.KEY_G));
-
-        ActionListener acl = new ActionListener() {
-
-            public void onAction(String name, boolean keyPressed, float tpf) {
-                
-                if(name.equals("rawXYdisplay") && keyPressed){
-                    PathXYDataDisplay probeData = PathXYDataDisplay.obtainXYProbeData(initialImportDirectory);
-                    rootNode.attachChild(probeData.generateSpatial(orangeMaterial));
-                    rootNode.attachChild(probeData.generateReferenceObject(greenMaterial));
-                }
-                
-                if(name.equals("rawYawPitchRollDisplay") && keyPressed){
-                    PathYawPitchRollDataDisplay probeData = PathYawPitchRollDataDisplay.obtainYawPitchRollProbeData(initialImportDirectory);
-                    rootNode.attachChild(probeData.generateSpatial(redMaterial));
-                    rootNode.attachChild(probeData.generateReferenceObject(greenMaterial));
-                }
-            }
-
-            
-        };
-
-        inputManager.addListener(acl,
-                "rawXYdisplay",
-                "rawYawPitchRollDisplay");
-
     }
 }
