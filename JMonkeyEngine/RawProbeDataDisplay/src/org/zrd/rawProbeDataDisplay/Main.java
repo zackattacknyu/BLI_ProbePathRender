a497ac90c6757b5cb442c142e0c970efe334c4b7
zackattacknyu <zackattacknyu@gmail.com>
2014-07-15 16:51:05 -0700

    put new application init code into other app

--- JMonkeyEngine/RawProbeDataDisplay/src/org/zrd/rawProbeDataDisplay/Main.java
+++ JMonkeyEngine/RawProbeDataDisplay/src/org/zrd/rawProbeDataDisplay/Main.java
@@ -2,11 +2,11 @@ package org.zrd.rawProbeDataDisplay;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.renderer.RenderManager;
-import com.jme3.system.AppSettings;
 import java.io.File;
 import java.nio.file.Paths;
 import java.util.Properties;
 import org.zrd.geometryToolkit.geometryUtil.ProgramConstants;
+import org.zrd.jmeUtil.applicationHelp.ApplicationHelper;
 import org.zrd.rawProbeDataDisplay.rawDataRendering.RawXYDataImport;
 import org.zrd.rawProbeDataDisplay.rawDataRendering.RawYawPitchRollDataImport;
 
@@ -17,31 +17,8 @@ import org.zrd.rawProbeDataDisplay.rawDataRendering.RawYawPitchRollDataImport;
 public class Main extends SimpleApplication {
 
     public static void main(String[] args) {
-        /*
-         * Refer to these two web pages to find out about start settings:
-         *      http://hub.jmonkeyengine.org/wiki/doku.php/jme3:intermediate:appsettings
-         * 
-         *      http://hub.jmonkeyengine.org/wiki/doku.php/jme3:intermediate:simpleapplication
-         * 
-         * 
-         */
         Properties appProps = Properties_RawProbeDataDisplay.getProperties();
-        AppSettings settings = new AppSettings(true);
-        
-        //adjusts title shown on window
-        settings.setTitle(appProps.getProperty("settings.title"));
-        
-        //adjusts resolution
-        int xRes = Integer.parseInt(appProps.getProperty("settings.xResolution"));
-        int yRes = Integer.parseInt(appProps.getProperty("settings.yResolution"));
-        settings.setResolution(xRes, yRes);
-        
-        Main app = new Main();
-        app.setSettings(settings); //disables the setting screen at start-up
-        app.setShowSettings(false); //shows the above settings
-        app.setDisplayFps(false); //makes sure the fps text is not displayed
-        app.setDisplayStatView(false); //makes sure the stat view is not displayed
-        app.start();
+        ApplicationHelper.initializeApplication(new Main(), appProps);
     }
     private File initialImportDirectory;
 
