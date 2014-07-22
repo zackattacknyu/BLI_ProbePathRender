ec506c06b796f9991113124ba73b70ef37b6cda3
zackattacknyu <zackattacknyu@gmail.com>
2014-07-22 04:20:01 -0700

    began commenting the serial interpreter

--- JMonkeyEngine/LibGeneral_SerialDataInterpreter/src/org/zrd/serialDataInterpreter/dataInterpretation/SerialDataInterpreter.java
+++ JMonkeyEngine/LibGeneral_SerialDataInterpreter/src/org/zrd/serialDataInterpreter/dataInterpretation/SerialDataInterpreter.java
@@ -11,6 +11,18 @@ import org.zrd.util.dataHelp.BasicAngleHelper;
 import org.zrd.util.dataStreaming.ProbeDataStream;
 
 /**
+ * This class takes in a reader which gives it raw orientation and 
+ *      displacement readings and decides what to tell the tracker
+ *      which is keeping track of the probe's position
+ *      and orientation.
+ * 
+ * It also starts, stops, and passes information to the recording
+ *      method which records that data into text files when desired. 
+ * 
+ * The x,y displacement is just passed to the next level
+ * 
+ * The yaw,pitch,roll data gets converted from degrees to radians
+ *      before being passed to the next level
  * 
  * @author BLI
  */
@@ -21,44 +33,40 @@ public class SerialDataInterpreter implements ProbeDataStream{
     private SerialDataReader serial;
     private Path dataRecordingFilePath;
 
-    float deltaX = 0;
-    float deltaY = 0;
+    private float deltaX = 0;
+    private float deltaY = 0;
     private float outputYawRadians,outputPitchRadians,outputRollRadians;
     
     private boolean recording = false;
     private SerialDataRecorder currentDataRecorder;
 
-
+    /**
+     * Initializes the serial interpreter by initializing the serial reader 
+     *      which is at a layer below it
+     * @param props         The properties used to initialize the reader
+     */
     public SerialDataInterpreter(Properties props) {
         serial = new SerialDataReader(props);
         System.out.println("Waiting to receive input...");
     }
     
+    /**
+     * Initializes the serial reader by initializing the serial reader which is
+     *      a layer below it and also takes the the file path in the event
+     *      that the data will want to be recorded
+     * @param props     properties used to initialize reader
+     * @param filePath  folder path to put text files of probe data
+     */
     public SerialDataInterpreter(Properties props, Path filePath){
         this(props);
         dataRecordingFilePath = filePath;
     }
     
-    public SerialDataReader getReader(){
-        return serial;
-    }
-    
-    @Override
-    public void startStopRecording(){
-        if(recording){
-            currentDataRecorder.closeRecording();
-            recording = false;
-                
-        }else{   
-            currentDataRecorder = new SerialDataRecorder(dataRecordingFilePath);
-            recording = true;
-
-        }
-    }
-
     /**
      * This is meant to be called many times in a second
-     *      and it updates the data from the serial probe
+     *      and it takes the current data from the serial probe
+     *      and calls the methods that give us the current
+     *      x,y and yaw,pitch,roll
      */
     @Override
     public void updateData(){
@@ -85,11 +93,28 @@ public class SerialDataInterpreter implements ProbeDataStream{
         deltaX = serial.getDeltaX();
         deltaY = serial.getDeltaY();
 
+        /*
+         * if we are recording the data, this adds the current data
+         *      to the files being generated
+         */
         if(recording){
             currentDataRecorder.addLineToFiles(deltaX, deltaY, 
                     outputYawRadians, 
                     outputPitchRadians, 
                     outputRollRadians);
+        }
+    }
+    
+    @Override
+    public void startStopRecording(){
+        if(recording){
+            currentDataRecorder.closeRecording();
+            recording = false;
+                
+        }else{   
+            currentDataRecorder = new SerialDataRecorder(dataRecordingFilePath);
+            recording = true;
+
         }
     }
 
@@ -113,4 +138,7 @@ public class SerialDataInterpreter implements ProbeDataStream{
         return deltaY;
     }
    
+    public SerialDataReader getReader(){
+        return serial;
+    }
 }
