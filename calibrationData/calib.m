51ae3671a7ae89df24ae192efa930908558e84e0
zackattacknyu <zackattacknyu@gmail.com>
2014-07-22 15:19:47 -0700

    added virtual coordinates from lola mesh

--- calibrationData/calib.m
+++ calibrationData/calib.m
@@ -9,6 +9,12 @@ marker 2-3: 62 mm
 marker 2-4: 37 mm
 marker 3-4: 46 mm
 
+Virtual Coordinates:
+Marker 1: (-0.45941067,-2.0322227,-14.933813)
+Marker 2: (-4.1024504,-2.726573,-13.849037)
+Marker 3: (-0.43226802,-4.228998,-17.187346)
+Marker 4: (-4.0149,-4.7278976,-16.069866)
+
 
 
 This is the data for the Ball Mesh and its markers:
@@ -20,5 +26,11 @@ marker 1-4: 122 mm
 marker 2-3: 111 mm
 marker 2-4: 92 mm
 marker 3-4: 65 mm
+
+Virtual Coordinates:
+Marker 1:
+Marker 2:
+Marker 3:
+Marker 4:
 
 %}
