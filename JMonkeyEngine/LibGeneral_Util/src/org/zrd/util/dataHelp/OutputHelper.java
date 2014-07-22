d77c337fb92ce32336c6013bac6669f21e867559
zackattacknyu <zackattacknyu@gmail.com>
2014-07-22 04:11:13 -0700

    finished adding comments to output helper

--- JMonkeyEngine/LibGeneral_Util/src/org/zrd/util/dataHelp/OutputHelper.java
+++ JMonkeyEngine/LibGeneral_Util/src/org/zrd/util/dataHelp/OutputHelper.java
@@ -5,16 +5,29 @@
 package org.zrd.util.dataHelp;
 
 /**
+ * These are simple methods that take in float values and string
+ *      them together to make a string for output purposes
  *
  * @author BLI
  */
 public class OutputHelper {
     /**
      * In each line of the output, this is the character
-     *      that separates each value
+     *      that separates each value. Change this variable
+     *      if you want a semicolon or other separator instead
+     *      of the current one.
      */
     public static final String DATA_IN_OUTPUT_SEPARATOR = ",";
     
+    /**
+     * This takes in a name of a field and its value and makes a string
+     *      in the form "{name}={value}  "
+     * There are two spaces afterwards
+     * 
+     * @param name      name of field
+     * @param value     its value
+     * @return          string in form "{name}={value}  "
+     */
     public static String makeNameValueDisplay(String name, float value){
         return name + "=" + value + "  ";
     }
