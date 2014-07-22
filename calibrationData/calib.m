93999921d33ea607f9c7e84ff9d1f9573f9039c9
zackattacknyu <zackattacknyu@gmail.com>
2014-07-22 15:07:26 -0700

    put lola data into matlab script

---
+++ calibrationData/calib.m
@@ -0,0 +1,12 @@
+%{
+This is the data for the Lola Mesh and its markers:
+
+Real-world distances:
+marker 1-2: 46 mm
+marker 1-3: 39 mm
+marker 1-4: 56 mm
+marker 2-3: 62 mm
+marker 2-4: 37 mm
+marker 3-4: 46 mm
+
+%}
