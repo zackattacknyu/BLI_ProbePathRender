a4e0b41b583948fde8887979559830abe6631af2
zackattacknyu <zackattacknyu@gmail.com>
2014-07-22 15:09:23 -0700

    added data from ball

--- calibrationData/calib.m
+++ calibrationData/calib.m
@@ -9,4 +9,16 @@ marker 2-3: 62 mm
 marker 2-4: 37 mm
 marker 3-4: 46 mm
 
+
+
+This is the data for the Ball Mesh and its markers:
+
+Real-world distances:
+marker 1-2: 75.5 mm
+marker 1-3: 94 mm
+marker 1-4: 122 mm
+marker 2-3: 111 mm
+marker 2-4: 92 mm
+marker 3-4: 65 mm
+
 %}
