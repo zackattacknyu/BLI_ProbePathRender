fb3a9c9d2e774ca23f86dec5b1b4ef2f386a1fab
zackattacknyu <zackattacknyu@gmail.com>
2014-07-22 23:54:44 -0700

    organized the application helper code better

--- JMonkeyEngine/LibJME_JMEUtil/src/org/zrd/jmeUtil/applicationHelp/ApplicationHelper.java
+++ JMonkeyEngine/LibJME_JMEUtil/src/org/zrd/jmeUtil/applicationHelp/ApplicationHelper.java
@@ -13,6 +13,11 @@ import java.util.Properties;
  *      Specifically, it sets up the default settings
  *      and calls the methods to get the other default settings
  *      from the .properties file
+ * 
+ * Code here will affect all the JME applications. If a change
+ *      should be made to all of them, then it should be
+ *      done in this class.
+ * 
 *  Refer to these two web pages to find out about start settings:
 *      http://hub.jmonkeyengine.org/wiki/doku.php/jme3:intermediate:appsettings
 * 
@@ -22,35 +27,84 @@ import java.util.Properties;
  * @author BLI
  */
 public class ApplicationHelper {
-
-    public static AppSettings getAppSettings(Properties appProps){
-        AppSettings settings = new AppSettings(true);
-        
-        //adjusts title shown on window
-        settings.setTitle(appProps.getProperty("settings.title"));
-        
-        //adjusts resolution
-        int xRes = Integer.parseInt(appProps.getProperty("settings.xResolution"));
-        int yRes = Integer.parseInt(appProps.getProperty("settings.yResolution"));
-        settings.setResolution(xRes, yRes);
-        
-        return settings;
-    }
     
+    /*
+     * These constants will affect all the JME applications
+     */
+    
+    //whether or not to dispaly the app settings in the window
+    public static final boolean SHOW_SETTINGS_IN_WINDOW = false;
+    
+    //whether or not to display the fps (frame per second) text
+    public static final boolean DISPLAY_FPS_IN_WINDOW = false;
+    
+    //whether or not to display the stat view in the window
+    public static final boolean DISPLAY_STAT_VIEW_IN_WINDOW = false;
+    
+    /*
+     * These constants are used for reading the properties file
+     *      with the settings
+     */
+    public static final String TITLE_PROPERTY_NAME = "settings.title";
+    public static final String X_RES_PROPERTY_NAME = "settings.xResolution";
+    public static final String Y_RES_PROPERTY_NAME = "settings.yResolution";
+    
+    /**
+     * This initializes an application using the application settings
+     * @param app           the application to start
+     * @param settings      the appsettings to use for it
+     */
     public static void initializeApplication(SimpleApplication app, AppSettings settings){
         
-        app.setSettings(settings); //disables the setting screen at start-up
-        app.setShowSettings(false); //shows the above settings
-        app.setDisplayFps(false); //makes sure the fps text is not displayed
-        app.setDisplayStatView(false); //makes sure the stat view is not displayed
+        /*
+         * This disables the setting screen in the beginning
+         *      from appearing
+         */
+        app.setSettings(settings); 
+        
+        //says whether or not to display certain stats
+        app.setShowSettings(SHOW_SETTINGS_IN_WINDOW); 
+        app.setDisplayFps(DISPLAY_FPS_IN_WINDOW); 
+        app.setDisplayStatView(DISPLAY_STAT_VIEW_IN_WINDOW); 
+        
+        //starts the application
         app.start();
         
     }
     
+    /**
+     * This intializes an application using the properties file that contain
+     *      the important settings
+     * @param app           the application to start
+     * @param appProps      the proerties object containing the important info
+     */
     public static void initializeApplication(SimpleApplication app, Properties appProps){
         
         initializeApplication(app,getAppSettings(appProps));
         
     }
+
+    /**
+     * This gets the application settings, which in this case
+     *      come from the .properties file
+     * @param appProps      properties object derived from properties file
+     * @return              AppSettings object for application settings derived from file
+     */
+    public static AppSettings getAppSettings(Properties appProps){
+        
+        AppSettings settings = new AppSettings(true);
+        
+        //adjusts title shown on window
+        settings.setTitle(appProps.getProperty(TITLE_PROPERTY_NAME));
+        
+        //adjusts resolution
+        int xRes = Integer.parseInt(appProps.getProperty(X_RES_PROPERTY_NAME));
+        int yRes = Integer.parseInt(appProps.getProperty(Y_RES_PROPERTY_NAME));
+        settings.setResolution(xRes, yRes);
+        
+        return settings;
+    }
+    
+    
     
 }
