5c368c720c03997741c9cce6052afb421385c3ae
zackattacknyu <zackattacknyu@gmail.com>
2014-07-27 19:01:10 -0700

    added comments to probe move actions

--- JMonkeyEngine/LibJME_JMEGeometryToolkit/src/org/zrd/jmeGeometryInteractions/meshPathInteractions/LineMoveAction.java
+++ JMonkeyEngine/LibJME_JMEGeometryToolkit/src/org/zrd/jmeGeometryInteractions/meshPathInteractions/LineMoveAction.java
@@ -17,7 +17,10 @@ import org.zrd.geometryToolkit.meshTraversal.ScaleCalibration;
 import org.zrd.geometryToolkit.pathDataStructure.RecordedPathSet;
 
 /**
- * 
+ * This handles moving a line so its start and end point match
+ *      with two points picked on the mesh. It is meant to be used
+ *      to move a pre-recorded or imported line between
+ *      two points and then show the transformation results.
  *
  * @author BLI
  */
@@ -26,37 +29,113 @@ public class LineMoveAction extends PickTwoPointsOnMesh{
     private RecordedPathSet recordedPathSet;
     private ArrayList<Vector3f> currentPath;
     
+    /**
+     * Name of the action hitting the keyboard to signify
+     *      moving the current line
+     */
+    public static final String LINE_MOVE_ACTION_NAME = "lineMoveAction";
+    
+    /**
+     * Name of the action for hitting the mouse to signify either a start
+     *      or end point for the path
+     */
+    public static final String POINT_CLICK_FOR_LINE_MOVE_ACTION_NAME = "pickPointForLineMove";
+    
+    /**
+     * The keyboard key to trigger the line move action
+     */
+    public static final int LINE_MOVE_ACTION_KEY = KeyInput.KEY_L;
+    
+    /**
+     * This message is displayed to the user to tell them that line moving 
+     *      has been enabled
+     */
+    public static final String LINE_MOVE_ENABLING_MESSAGE = "Last Line will be moved to next 2 points clicked";
+    
+    /**
+     * This message is displayed to the user to tell them that line moving 
+     *      has been cancelled
+     */
+    public static final String LINE_MOVE_CANCEL_MESSAGE = "Line Moving Cancelled";
+    
+    /**
+     * This initializes the line move action
+     * @param inputManager          application's input manager
+     * @param cam                   the application's camera
+     * @param shootableMesh         the mesh that contains the two points
+     * @param recordedPathSet       the set of recorded paths from the application
+     * @param meshInfo              the triangleSet of the mesh
+     */
     public LineMoveAction(InputManager inputManager, Camera cam, Node shootableMesh, RecordedPathSet recordedPathSet, TriangleSet meshInfo){
-        super("lineMoveAction","pickPointForLineMove",KeyInput.KEY_L,inputManager,cam,shootableMesh,meshInfo);
+        super(LINE_MOVE_ACTION_NAME,POINT_CLICK_FOR_LINE_MOVE_ACTION_NAME,
+                LINE_MOVE_ACTION_KEY,inputManager,cam,shootableMesh,meshInfo);
         this.recordedPathSet = recordedPathSet;
     }
     
+    /**
+     * This returns a string saying that line moving has been enabled
+     * @return      string for the console saying line moving enabled
+     */
     @Override
     protected String messageUponEnabling() {
-        return "Last Line will be moved to next 2 points clicked";
+        return LINE_MOVE_ENABLING_MESSAGE;
     }
 
+    /**
+     * This returns a string saying line moving was cancelled
+     * @return      string saying line moving cancelled
+     */
     @Override
     protected String messageUponCancelling() {
-        return "Line Moving Cancelled";
+        return LINE_MOVE_CANCEL_MESSAGE;
     }
 
+    /**
+     * This gets called when the start point has been clicked and it
+     *      moves the path start point to the clicked point
+     * @param startPoint        the clicked point
+     */
     @Override
     protected void handleStartPoint(Vector3f startPoint) {
+        
+        //gets the current pre-recorded path
         currentPath = recordedPathSet.getCurrentPath();
+        
+        //moves the current path so its start point matches the one given
         currentPath = MeshTraverseHelper.movePathStartPoint(currentPath, startPoint);
     }
 
+    /**
+     * This gets called when the end point has been clicked and it 
+     *      sets the current path to the one found in the superclass
+     *      that was scaled and rotated. 
+     * @param endPoint                  the end point clicked on
+     * @param scaleCalib                the scale calibration
+     * @param rotCalib                  the rotation calibration
+     * @param scaledAndRotatedPath      the scaled and rotated path from the superclass
+     */
     @Override
-    protected void handleEndPointResult(Vector3f endPoint, ScaleCalibration scaleCalib, RotationCalibration rotCalib, ArrayList<Vector3f> scaledAndRotatedPath) {
+    protected void handleEndPointResult(Vector3f endPoint, 
+        ScaleCalibration scaleCalib, 
+        RotationCalibration rotCalib, 
+        ArrayList<Vector3f> scaledAndRotatedPath) {
+        
         currentPath = scaledAndRotatedPath;
     }
 
+    /**
+     * This returns the current path to the superclass
+     * @return      the current path as list of vertices
+     */
     @Override
     protected ArrayList<Vector3f> getActivePathAtEndpoint() {
         return currentPath;
     }
     
+    /**
+     * returns the current active path as list of vertices
+     * @return  current path as list of vertices
+     */
     public ArrayList<Vector3f> getCurrentPath() {
         return currentPath;
     }
--- JMonkeyEngine/LibJME_JMEGeometryToolkit/src/org/zrd/jmeGeometryInteractions/meshPathInteractions/PickTwoPointsOnMesh.java
+++ JMonkeyEngine/LibJME_JMEGeometryToolkit/src/org/zrd/jmeGeometryInteractions/meshPathInteractions/PickTwoPointsOnMesh.java
@@ -22,6 +22,13 @@ import org.zrd.jmeGeometryIO.meshIO.MeshInputHelper;
 import org.zrd.jmeUtil.mouseKeyboard.GeneralKeyboardActionMethod;
 
 /**
+ * 
+ * This is an abstract class which handles picking two points on the mesh
+ *      which correspond to desired start and end points for a path.
+ * It handles calling the rotation, scaling, and translation methods
+ *      required for the transformation which the subclasses 
+ *      are in charge of getting the path in the first place and handling
+ *      the transformed path
  *
  * @author BLI
  */
@@ -36,61 +43,143 @@ public abstract class PickTwoPointsOnMesh extends GeneralKeyboardActionMethod im
     
     private boolean pointsNewlyPicked = false;
     
-    public PickTwoPointsOnMesh(String actionName, String pointPickName, int keyTrigger, InputManager inputManager, Camera cam, Node shootableMesh, TriangleSet meshInfo){
+    /**
+     * This initializes the class that picks two points on the mesh
+     * @param actionName            name of the keyboard action that requires picking two points
+     * @param pointPickName         name of the mouse action that picks a point
+     * @param keyTrigger            the keyboard trigger to start the action
+     * @param inputManager          the application's input manager
+     * @param cam                   the application's camera
+     * @param shootableMesh         the mesh where the points will occur on
+     * @param meshInfo              the TriangleSet from Geometry toolkit for the mesh
+     */
+    protected PickTwoPointsOnMesh(String actionName, String pointPickName, int keyTrigger, InputManager inputManager, Camera cam, Node shootableMesh, TriangleSet meshInfo){
         super(inputManager,actionName,keyTrigger);
         new PickPointOnMesh(pointPickName,inputManager,cam,this,shootableMesh);
         this.meshInfo = meshInfo;
     }
     
+    /**
+     * This gets called when the keyboard key has been pressed saying
+     *      that the two point clicking has been enabled
+     */
     @Override
     public void actionMethod() {
+        
+        //enables the clicking of two points
         if(!twoPointPickEnabled){
             System.out.println(messageUponEnabling());
             twoPointPickEnabled = true;
         }else{
+            
+            //if key is pressed before two points are picked, then it cancels the action
             System.out.println(messageUponCancelling());
             twoPointPickEnabled = false;
             onStartPoint = true;
         }
     }
     
+    /**
+     * This is meant to be implemented by a subclass and it is the message
+     *      put in the console when enabling the two points clicking action
+     * @return      message for the console to say action is enabled
+     */
     protected abstract String messageUponEnabling();
+    
+    /**
+     * This is meant to be implemented by a subclass and it is the message
+     *      put in the console when cancelling the two points clicking action
+     * @return      message for the console to say action is cancelled
+     */
     protected abstract String messageUponCancelling();
 
+    /**
+     * This is meant to be called by a subclass and it is passed the start
+     *      point that is clicked and it is supposed to do whatever
+     *      action is necessary depending on that start point
+     * @param startPoint    the initial point clicked
+     */
     protected abstract void handleStartPoint(Vector3f startPoint);
-    protected abstract void handleEndPointResult(Vector3f endPoint,ScaleCalibration scaleCalib, RotationCalibration rotCalib, ArrayList<Vector3f> scaledAndRotatedPath);
+    
+    /**
+     * This is meant to be called by a subclass and it is passed the end
+     *      point that is clicked as well as the rotation and scale
+     *      calibration object results. It does whatever action is necessary
+     *      based on those results
+     * @param endPoint                  the end point clicked
+     * @param scaleCalib                the scale calibration object
+     * @param rotCalib                  the rotation calibration object
+     * @param scaledAndRotatedPath      the transformed path
+     */
+    protected abstract void handleEndPointResult(Vector3f endPoint,
+            ScaleCalibration scaleCalib, 
+            RotationCalibration rotCalib, 
+            ArrayList<Vector3f> scaledAndRotatedPath);
+    
+    /**
+     * This must be called by a subclass and it gets whatever path
+     *      is the one we are about when we click the endpoint
+     * @return 
+     */
     protected abstract ArrayList<Vector3f> getActivePathAtEndpoint();
     
+    /**
+     * This gets the MeshTriangle at the end point
+     * @return  meshTriangle at end point
+     */
     public MeshTriangle getEndingTriangle(){
         return endingTriangle;
     }
 
+    /**
+     * This takes in the point on the mesh and the triangle at that point
+     *      and does various actions depending on whether it is the start or end
+     *      point. At the start point, it calls the methods that handle the start
+     *      point. At the end point, it does the calibrations and handles the 
+     *      results. 
+     * 
+     * @param pointOnMesh       the point on the mesh
+     * @param triangleOnMesh    the triangle at that point
+     */
     @Override
     public void handleNewMeshPoint(Vector3f pointOnMesh, Triangle triangleOnMesh) {
+        
+        //makes sure the action has been enabled
         if(twoPointPickEnabled){
-                                
+            
+            //we are at the start point
             if(onStartPoint){
 
                 Vector3f startPoint = pointOnMesh.clone();
                 if(!startPoint.equals(lastPointClicked)){
 
                     lastPointClicked = startPoint;
+                    
+                    //registers the starting triangle
                     startingTriangle = MeshInputHelper.convertInputTriangleToMeshTriangle(triangleOnMesh, meshInfo.getTransform());
+                    
+                    //calls the action that handles the start point
                     handleStartPoint(startPoint);
+                    
                     onStartPoint = false;
                 }
 
             }else{
+                
+                //we are at the end point
 
                 Vector3f endPoint = pointOnMesh.clone();
                 if(!endPoint.equals(lastPointClicked)){
 
                     lastPointClicked = endPoint;
                     
+                    //registers the ending triangle
                     endingTriangle = MeshInputHelper.convertInputTriangleToMeshTriangle(triangleOnMesh, meshInfo.getTransform());
                     
+                    //gets the active pre-transformed path
                     ArrayList<Vector3f> activePath = getActivePathAtEndpoint();
                     
+                    //gets the calibrations
                     ScaleCalibration currentScaleCalib = new ScaleCalibration(activePath,endPoint);
                     //activePath = currentScaleCalib.getScaledPath();
                     activePath = PathCompression.getCompressedPath(activePath,ProgramConstants.MIN_SEGMENT_LENGTH);
@@ -98,6 +187,7 @@ public abstract class PickTwoPointsOnMesh extends GeneralKeyboardActionMethod im
                             new RotationCalibration(activePath,endPoint,startingTriangle,meshInfo);
                     activePath = currentRotCalib.getCurrentPathOnSurface();
                     
+                    //calls the method that handles the result
                     handleEndPointResult(endPoint,currentScaleCalib,currentRotCalib,activePath);
                     
                     twoPointPickEnabled = false;
@@ -109,6 +199,12 @@ public abstract class PickTwoPointsOnMesh extends GeneralKeyboardActionMethod im
         }
     }
     
+    /**
+     * This says whether the points were newly chosen. It is meant to only 
+     *      return true once for each time a point is chosen, thus once the variable
+     *      is true, this method returns true but resets it to false.
+     * @return      whether or not points have just been chosen
+     */
     public boolean arePointsNewlyPicked(){
         if(pointsNewlyPicked){
             pointsNewlyPicked = false;
