63848b02ee58eabe199da2ee270294f44e46e700
zackattacknyu <zackattacknyu@gmail.com>
2014-07-27 20:06:55 -0700

    modified point pick recording slightly

--- JMonkeyEngine/JMEApplication_LiveProbeTracking/src/org/zrd/bliProbePath/Main.java
+++ JMonkeyEngine/JMEApplication_LiveProbeTracking/src/org/zrd/bliProbePath/Main.java
@@ -130,7 +130,8 @@ public class Main extends SimpleApplication {
                 inputManager, cam, shootables, probeTracker, meshInfo,
                 Paths_BLIProbePath.CALIBRATION_RESULTS_PATH);
         probeTrackerOnSurface = new ProbeTrackerOnSurface(probeTracker,rotCalib,meshInfo);
-        PickAndRecordPoint pointRecorder = new PickAndRecordPoint(inputManager,cam,shootables,Paths_BLIProbePath.PATH_RECORDING_PATH);
+        PickAndRecordPoint pointRecorder = new PickAndRecordPoint(inputManager,cam,
+                shootables,Paths_BLIProbePath.PATH_RECORDING_PATH, activeMesh.getActiveMeshInfo().getTransform());
         
         activeTracker = surfaceTrackingOn ? probeTrackerOnSurface : probeTracker;
         
--- JMonkeyEngine/LibJME_JMEGeometryToolkit/src/org/zrd/jmeGeometryInteractions/meshInteraction/PickAndRecordPoint.java
+++ JMonkeyEngine/LibJME_JMEGeometryToolkit/src/org/zrd/jmeGeometryInteractions/meshInteraction/PickAndRecordPoint.java
@@ -7,6 +7,7 @@ package org.zrd.jmeGeometryInteractions.meshInteraction;
 import org.zrd.geometryToolkit.geometryUtil.MeshPointHandler;
 import com.jme3.input.InputManager;
 import com.jme3.input.KeyInput;
+import com.jme3.math.Matrix4f;
 import com.jme3.math.Triangle;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.Camera;
@@ -14,6 +15,8 @@ import com.jme3.scene.Node;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import org.zrd.geometryToolkit.meshDataStructure.MeshTriangle;
+import org.zrd.jmeGeometryIO.meshIO.MeshInputHelper;
 import org.zrd.jmeUtil.mouseKeyboard.GeneralKeyboardActionMethod;
 import org.zrd.util.dataWriting.ProbeDataWriter;
 import org.zrd.util.fileHelper.FileDataHelper;
@@ -26,11 +29,13 @@ public class PickAndRecordPoint extends GeneralKeyboardActionMethod implements M
 
     private boolean pickPointEnabled = false;
     private Path recordedFilePath;
+    private Matrix4f triangleTransform;
     
-    public PickAndRecordPoint(InputManager inputManager, Camera cam, Node shootableMesh, Path recordedFilePath){
+    public PickAndRecordPoint(InputManager inputManager, Camera cam, Node shootableMesh, Path recordedFilePath, Matrix4f triangleTransform){
         super(inputManager,"pickAndRecordPoint",KeyInput.KEY_P);
         new PickPointOnMesh("pickPointForRecording",inputManager,cam,this,shootableMesh);
         this.recordedFilePath = recordedFilePath;
+        this.triangleTransform = triangleTransform;
     }
     
     @Override
@@ -46,13 +51,36 @@ public class PickAndRecordPoint extends GeneralKeyboardActionMethod implements M
     @Override
     public void handleNewMeshPoint(Vector3f pointOnMesh, Triangle triangleOnMesh) {
         ArrayList<String> fileStrings = new ArrayList<String>(10);
-        fileStrings.add("Point On Mesh: ");
-        fileStrings.add(String.valueOf(pointOnMesh));
-        fileStrings.add(" ");
-        fileStrings.add("Current Triangle:");
-        fileStrings.add(String.valueOf(triangleOnMesh.get1()));
-        fileStrings.add(String.valueOf(triangleOnMesh.get2()));
-        fileStrings.add(String.valueOf(triangleOnMesh.get3()));
+        ArrayList<String> pointStrings = new ArrayList<String>(10);
+        ArrayList<String> currentTriStrings = new ArrayList<String>(10);
+        ArrayList<String> origTriStrings = new ArrayList<String>(10);
+        
+        pointStrings.add("Point On Mesh: ");
+        pointStrings.add(String.valueOf(pointOnMesh));
+        
+        /*
+         * TODO: Fix why this block is not giving the original triangle
+         * 
+        origTriStrings.add(" ");
+        origTriStrings.add("Triangle On Original Mesh:");
+        origTriStrings.add(String.valueOf(triangleOnMesh.get1()));
+        origTriStrings.add(String.valueOf(triangleOnMesh.get2()));
+        origTriStrings.add(String.valueOf(triangleOnMesh.get3()));
+        */
+
+        MeshTriangle meshTri = MeshInputHelper.convertInputTriangleToMeshTriangle(triangleOnMesh, triangleTransform);
+        currentTriStrings.add(" ");
+        currentTriStrings.add("Triangle On Current Mesh: ");
+        currentTriStrings.add(String.valueOf(meshTri.getVertex1()));
+        currentTriStrings.add(String.valueOf(meshTri.getVertex2()));
+        currentTriStrings.add(String.valueOf(meshTri.getVertex3()));
+        
+        //decide the order in which to put the strings
+        fileStrings.addAll(pointStrings);
+        fileStrings.addAll(currentTriStrings);
+        fileStrings.addAll(origTriStrings);
+        
+        
         try {
             Path outputFilePath = ProbeDataWriter.getNewDataFilePath(recordedFilePath, "meshPointPicked");
             FileDataHelper.exportLinesToFile(fileStrings, outputFilePath);
