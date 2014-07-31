bac3912156ec32a5f86348a9d9d1aa25b5f6d682
zackattacknyu <zackattacknyu@gmail.com>
2014-07-31 14:43:22 -0700

    made new project for mesh display

---
+++ JMonkeyEngine/JMEApplication_MeshDisplay/src/org/zrd/rawProbeDataDisplay/Main.java
@@ -0,0 +1,40 @@
+package org.zrd.rawProbeDataDisplay;
+
+import com.jme3.app.SimpleApplication;
+import com.jme3.renderer.RenderManager;
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Properties;
+import org.zrd.jmeUtil.applicationHelp.ApplicationHelper;
+
+/**
+ * test
+ * @author normenhansen
+ */
+public class Main extends SimpleApplication {
+
+    public static void main(String[] args) {
+        Properties appProps = Properties_RawProbeDataDisplay.getProperties();
+        ApplicationHelper.initializeApplication(new Main(), appProps);
+    }
+    private File initialImportDirectory;
+
+    @Override
+    public void simpleInitApp() {
+        
+        viewPort.setBackgroundColor(ApplicationHelper.BACKGROUND_COLOR);
+        initialImportDirectory = Paths.get("C:\\Users\\BLI\\Desktop\\BLI_ProbePathRender\\sampleTextFiles").toFile();
+        
+        new CameraTrackerImpl_RawProbeDataDisplay(cam,flyCam,inputManager);
+    }
+
+    @Override
+    public void simpleUpdate(float tpf) {
+        //TODO: add update code
+    }
+
+    @Override
+    public void simpleRender(RenderManager rm) {
+        //TODO: add render code
+    }
+}
