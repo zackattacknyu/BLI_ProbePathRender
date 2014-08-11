69d8919d2f026531314facae097bf303aaa92d5d
zackattacknyu <zackattacknyu@gmail.com>
2014-08-11 15:11:17 -0700

    added comments

--- JMonkeyEngine/JMEApplication_LiveProbeTracking/src/org/zrd/bliProbePath/FixedPointIO.java
+++ JMonkeyEngine/JMEApplication_LiveProbeTracking/src/org/zrd/bliProbePath/FixedPointIO.java
@@ -17,6 +17,16 @@ import org.zrd.geometryToolkit.pointTools.PointOnMeshData;
 import org.zrd.util.fileHelper.GeneralFileHelper;
 
 /**
+ * This imports and exports fixed points on the mesh to and from text files.
+ * 
+ * For fixed points on the mesh, we need to know the points as well
+ *      as their corresponding triangles
+ * 
+ * The text files with fixed points consist of sets of 4 vertices:
+ *      {fixed points coordinates}
+ *      {corresponding triangle vertex 1 coordinates}
+ *      {corresponding triangle vertex 2 coordinates}    
+ *      {corresponding triangle vertex 3 coordinates}    
  *
  * @author Zach
  */
@@ -25,15 +35,32 @@ public class FixedPointIO {
     private ArrayList<Vector3f> outputVertices;
     private Collection<PointData> fixedPointsOnMesh;
     
+    /**
+     * This initializes the object and is meant to be used when we
+     *      are exporting fixed points to a text file.
+     */
     public FixedPointIO(){
         outputVertices = new ArrayList<Vector3f>();
     }
     
+    /**
+     * This takes a file, reads it vertices, and then puts the
+     *      vertices into a list for the constructor to parse
+     *      through and get the fixed points
+     *      and corresponding triangles
+     * @param fileToImport      file with vertex data
+     * @return                  object which contains fixed points and their triangles
+     */
     public static FixedPointIO getPointsFromFile(File fileToImport){
         ArrayList<Vector3f> vertices = GeometryDataHelper.getVerticesFromFile(fileToImport);
         return new FixedPointIO(vertices);
     }
 
+    /**
+     * This takes a list of vertices and imports the fixed points
+     *      and corresponding triangles from the list
+     * @param outputVerts   list of vertices which have points and their triangles
+     */
     public FixedPointIO(ArrayList<Vector3f> outputVerts){
         
         fixedPointsOnMesh = new ArrayList<PointData>();
@@ -43,12 +70,23 @@ public class FixedPointIO {
         Vector3f currentFixedPt;
         MeshTriangle currentTriangle;
         
+        /*
+         * The vertices come in sets of 4 as described above, 
+         *      so there needs to be a multiple of 4 number
+         *      of vertices in the list
+         */
         if(numVerts % 4 == 0){
+            
+            //goes through each set of 4
             for(int i = 0; i < numVerts/4; i++){
                 
+                //gets the fixed point in the set
                 currentFixedPt = outputVerts.get(i*4).clone();
+                
+                //gets the corresponding triangle in the set
                 currentTriangle = new MeshTriangle(outputVerts.get(i*4 + 1),outputVerts.get(i*4 + 2),outputVerts.get(i*4 + 3));
                 
+                //puts the data into the collection
                 fixedPointsOnMesh.add(new PointOnMeshData(currentFixedPt,currentTriangle));
                 
             }
@@ -64,6 +102,12 @@ public class FixedPointIO {
         return new FixedPointPicker(fixedPointsOnMesh);
     }
     
+    /**
+     * This takes in a point and its triangle and adds it to the vertex output
+     *      list. 
+     * @param fixedPoint                the point on the mesh
+     * @param correspondingTriangle     the point's triangle on the mesh
+     */
     public void addPoint(Vector3f fixedPoint, MeshTriangle correspondingTriangle){
         outputVertices.add(fixedPoint);
         outputVertices.add(correspondingTriangle.getVertex1().getVertex());
@@ -71,6 +115,12 @@ public class FixedPointIO {
         outputVertices.add(correspondingTriangle.getVertex3().getVertex());
     }
     
+    /**
+     * This writes the output vertex data to a text file that can later
+     *      be read in by this same class and outputs the file object
+     * @param dataPath      path to put text file data
+     * @return              file object where data was written to
+     */
     public File writeInformationToFile(Path dataPath){
         System.out.println("Now putting vertices into text file");
         Path textFilePath = GeneralFileHelper.getNewDataFilePath(dataPath, "fixedPoints");
