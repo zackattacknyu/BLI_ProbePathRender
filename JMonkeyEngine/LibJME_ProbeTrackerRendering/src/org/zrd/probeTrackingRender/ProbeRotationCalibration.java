c8ea063fd5e876e4db3c8a9f9816758d7ea5936e
zackattacknyu <zackattacknyu@gmail.com>
2014-08-30 18:46:03 -0700

    added output message

--- JMonkeyEngine/LibJME_ProbeTrackerRendering/src/org/zrd/probeTrackingRender/ProbeRotationCalibration.java
+++ JMonkeyEngine/LibJME_ProbeTrackerRendering/src/org/zrd/probeTrackingRender/ProbeRotationCalibration.java
@@ -81,6 +81,7 @@ public class ProbeRotationCalibration extends PickTwoPointsOnMesh implements Rot
     protected void handleStartPoint(Vector3f startPoint) {
         System.out.println("Start Point set at: " + startPoint);
         System.out.println("Hit space bar before selecting the end point");
+        System.out.println("Press B to cancel and only produce the align normal calibration");
         probeTracker.setCurrentPosition(startPoint);
         probeTracker.startStopRecording();
 
