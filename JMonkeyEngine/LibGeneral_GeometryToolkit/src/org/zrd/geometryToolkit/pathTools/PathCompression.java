f8257f01807923acbc2fea22f67cbc43e4d4b050
zackattacknyu <zackattacknyu@gmail.com>
2014-09-29 15:40:14 -0700

    redid the compression code

--- JMonkeyEngine/LibGeneral_GeometryToolkit/src/org/zrd/geometryToolkit/pathTools/PathCompression.java
+++ JMonkeyEngine/LibGeneral_GeometryToolkit/src/org/zrd/geometryToolkit/pathTools/PathCompression.java
@@ -13,35 +13,39 @@ import org.zrd.geometryToolkit.pathDataStructure.SegmentSet;
  * @author BLI
  */
 public class PathCompression {
-
-    public static ArrayList<Vector3f> getCompressedPath(ArrayList<Vector3f> inputPath, float minSegmentLength) {
+    
+    public static ArrayList<Integer> getCompressedPathVertexIndices(ArrayList<Vector3f> inputPath, float minSegmentLength){
         Vector3f lastPointAdded = inputPath.get(0);
-        ArrayList<Vector3f> newPath = new ArrayList<Vector3f>(inputPath.size());
-        newPath.add(lastPointAdded);
+        ArrayList<Integer> newPathVertices = new ArrayList<Integer>(inputPath.size());
+        newPathVertices.add(0);
         float currentLength;
-        for (Vector3f vertex : inputPath) {
+        Vector3f vertex;
+        for(int index = 1; index < inputPath.size(); index++){
+            vertex = inputPath.get(index);
             currentLength = vertex.distance(lastPointAdded);
             if (currentLength > minSegmentLength) {
-                newPath.add(vertex);
+                newPathVertices.add(index);
                 lastPointAdded = vertex;
             }
+        }
+        return newPathVertices;
+    }
+
+    public static ArrayList<Vector3f> getCompressedPath(ArrayList<Vector3f> inputPath, float minSegmentLength) {
+        ArrayList<Integer> compressedPathVertexIndices = getCompressedPathVertexIndices(inputPath,minSegmentLength);
+        ArrayList<Vector3f> newPath = new ArrayList<Vector3f>(inputPath.size());
+        for(Integer index: compressedPathVertexIndices){
+            newPath.add(inputPath.get(index));
         }
         return newPath;
     }
     
     public static SegmentSet getCompressedPath(SegmentSet inputPath, float minSegmentLength) {
-        Vector3f lastPointAdded = inputPath.getDataAtIndex(0).getVertex();
+        ArrayList<Integer> compressedPathVertexIndices = 
+                getCompressedPathVertexIndices(inputPath.getPathVertices(),minSegmentLength);
         SegmentSet newPath = new SegmentSet(inputPath.getSize());
-        newPath.addToSet(inputPath.getDataAtIndex(0));
-        float currentLength;
-        Vector3f vertex;
-        for(int index = 1; index < inputPath.getSize(); index++){
-            vertex = inputPath.getDataAtIndex(index).getVertex();
-            currentLength = vertex.distance(lastPointAdded);
-            if (currentLength > minSegmentLength) {
-                newPath.addToSet(inputPath.getDataAtIndex(index));
-                lastPointAdded = vertex;
-            }
+        for(Integer index: compressedPathVertexIndices){
+            newPath.addToSet(inputPath.getDataAtIndex(index));
         }
         return newPath;
     }
