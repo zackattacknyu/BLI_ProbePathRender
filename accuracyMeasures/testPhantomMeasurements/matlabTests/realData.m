6138ea027918782c92001a55622752983069231d
zackattacknyu <zackattacknyu@gmail.com>
2014-12-16 01:07:25 -0800

    successfully calculated point coordinates for first 7 points

--- accuracyMeasures/testPhantomMeasurements/matlabTests/realData.m
+++ accuracyMeasures/testPhantomMeasurements/matlabTests/realData.m
@@ -30,3 +30,37 @@ ang_5_O_6 = getAngleFromSSS(len_O_5,len_O_6,len_5_6);
 ang_6_O_7 = getAngleFromSSS(len_O_6,len_O_7,len_6_7);
 ang_72_7_71 = getAngleFromSSS(len_7_72,len_7_71,len_71_72);
 ang_6_7_71 = getAngleFromSSS(len_6_7,len_7_71,len_6_71);
+ang_2_1_O = getAngleFromSSS(len_1_2,len_O_1,len_O_2);
+ang_6_7_O = getAngleFromSSS(len_O_7,len_6_7,len_O_6);
+
+%used to get x,y coordinates for points where we can use the
+%   following coordinate system:
+%   O is origin. line_O_1 is positive x-axis line
+r_1=len_O_1;
+theta_1 = 0;
+r_2=len_O_2;
+theta_2 = ang_1_O_2;
+r_3=len_O_3;
+theta_3 = ang_2_O_3 + theta_2;
+r_4=len_O_4;
+theta_4 = ang_3_O_4 + theta_3;
+r_5=len_O_5;
+theta_5 = ang_4_O_5 + theta_4;
+r_6=len_O_6;
+theta_6 = ang_5_O_6 + theta_5;
+r_7=len_O_7;
+theta_7 = ang_6_O_7 + theta_6;
+
+%gets x,y coordinates from above
+point_1=getXYFromRTheta(r_1,theta_1);
+point_2=getXYFromRTheta(r_2,theta_2);
+point_3=getXYFromRTheta(r_3,theta_3);
+point_4=getXYFromRTheta(r_4,theta_4);
+point_5=getXYFromRTheta(r_5,theta_5);
+point_6=getXYFromRTheta(r_6,theta_6);
+point_7=getXYFromRTheta(r_7,theta_7);
+
+
+
+
+
