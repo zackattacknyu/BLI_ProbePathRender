f1c2628456dcb659a15caea014c9cb4cf3db3585
zackattacknyu <zackattacknyu@gmail.com>
2014-12-23 13:52:41 -0800

    modified scripts more

--- accuracyMeasures/testPhantomMeasurements/matlabTests/realData2.m
+++ accuracyMeasures/testPhantomMeasurements/matlabTests/realData2.m
@@ -3,17 +3,7 @@
 point_O = [0 0];
 point_6 = [52.5 0]; %the positive x-axis
 
-startPt = point_O;
-otherPt = point_6;
-distFromStart = 56;
-distFromOther = 46.5;
-[x_5,y_5] = circcirc(startPt(1),startPt(2),distFromStart,otherPt(1),otherPt(2),distFromOther);
+dist_O_5 = 56;
+dist_5_6 = 46.5;
+point_5 = getPointFromTwoCircles(point_O,dist_O_5,point_6,dist_5_6,1);
 
-possPoint1 = [x_5(1) y_5(1)];
-possPoint2 = [x_5(2) y_5(2)];
-vector_Start_1 = possPoint1-startPt;
-vector_Start_2 = possPoint2-startPt;
-vector_Start_other = otherPt-startPt;
-
-leftFlag1 = getRightLeft(vector_Start_other,vector_Start_1);
-leftFlag2 = getRightLeft(vector_Start_other,vector_Start_2);
