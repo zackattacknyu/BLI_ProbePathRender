fc208065895d638fe86cf2de42feaaa40d2a44c1
zackattacknyu <zackattacknyu@gmail.com>
2014-12-23 18:23:09 -0800

    got other points

---
+++ accuracyMeasures/testPhantomMeasurements/matlabTests/getRealWorldData2.m
@@ -0,0 +1,69 @@
+function [ distsReal ] = getRealWorldData2( )
+%GETREALWORLDDATA2 Summary of this function goes here
+%   Detailed explanation goes here
+
+%length between points
+len_O_1=25.5;
+len_O_2=51;
+len_O_3=50;
+len_O_4=59;
+len_O_5=56;
+len_O_6=52.5;
+len_O_7=25.5;
+len_1_11=39.5;
+len_1_12=40;
+len_7_71=18.5;
+len_7_72=41;
+len_11_12=24.5;
+len_12_2=26.5;
+len_1_2=36;
+len_2_3=49.5;
+len_3_4=25.5;
+len_4_5=25.5;
+len_5_6=46.5;
+len_6_7=32;
+len_71_72=26.5;
+len_6_71=24.5;
+
+%gets the real coordinates using circle-circle intersections
+%       instead of polar to cartesian conversion
+point_O = [0 0];
+point_6 = [len_O_6 0]; %the positive x-axis
+
+%point 5 is to the left of the O-6 line
+point_5 = getPointFromTwoCircles(point_O,len_O_5,point_6,len_5_6,1);
+
+%point 7 is to the right of the O-6 line
+point_7 = getPointFromTwoCircles(point_O,len_O_7,point_6,len_6_7,0);
+
+%point 71 is to the right of the 7-6 line
+point_71 = getPointFromTwoCircles(point_7,len_7_71,point_6,len_6_71,0);
+
+%point 72 is to the right of the 7-71 line
+point_72 = getPointFromTwoCircles(point_7,len_7_72,point_71,len_71_72,0);
+
+%point 4 is to the left of the O-5 line
+point_4 = getPointFromTwoCircles(point_O,len_O_4,point_5,len_4_5,1);
+
+%point 3 is to the left of the O-4 line
+point_3 = getPointFromTwoCircles(point_O,len_O_3,point_4,len_3_4,1);
+
+%point 2 is to the left of the O-3 line
+point_2 = getPointFromTwoCircles(point_O,len_O_2,point_3,len_2_3,1);
+
+%point 1 is to the left of the O-2 line
+point_1 = getPointFromTwoCircles(point_O,len_O_1,point_2,len_1_2,1);
+
+%point 12 is to the left of the 1-2 line
+point_12 = getPointFromTwoCircles(point_1,len_1_12,point_2,len_12_2,1);
+
+%point 11 is to the left of the 1-12 line
+point_11 = getPointFromTwoCircles(point_1,len_1_11,point_12,len_11_12,1);
+
+
+points = [point_O;point_1;point_2;point_3;point_4;point_5;point_6;point_7;point_12;point_11;point_71;point_72];
+distsReal = getPairwise(points);
+
+
+end
+
--- accuracyMeasures/testPhantomMeasurements/matlabTests/runTests.m
+++ accuracyMeasures/testPhantomMeasurements/matlabTests/runTests.m
@@ -4,7 +4,7 @@ load 'fixedPointsCoords_testPhantom3.txt'
 distsVirtual1 = getPairwise(fixedPointsCoords_testPhantom1);
 distsVirtual3 = getPairwise(fixedPointsCoords_testPhantom3);
 
-distsReal = getRealWorldData();
+distsReal = getRealWorldData2();
 
 ratios1 = getRatiosArray(distsReal,distsVirtual1);
 meanRatios1 = mean(ratios1);
@@ -21,3 +21,8 @@ stdPercentError13 = std(percentError13);
 percentError31 = getPercentErrorArray(distsVirtual3,distsVirtual1);
 meanPercentError31 = mean(percentError31);
 stdPercentError31 = std(percentError31);
+
+distsReal2 = getRealWorldData();
+percentErrorDists = getPercentErrorArray(distsReal,distsReal2);
+meanPercentErrorDists = mean(percentErrorDists);
+stdPercentErrorDists = std(percentErrorDists);
